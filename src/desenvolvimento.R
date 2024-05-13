eba008745d74a245d7b9c76d7d51b159489c455f
Armando Nahmias <armando@informartizar.com>
2024-05-13 11:37:10 -0400

    ajustes

--- src/desenvolvimento.R
+++ src/desenvolvimento.R
@@ -1,11 +1,59 @@
-lista.recursos <- c(
-          'arquivos',
-          # 'empenhos',
-          'faturas',
-          'garantias',
-          # 'itens',
-          'publicacoes',
-          'terceirizados'
-)
 
-for (recurso in lista.recursos) consultar.recurso(recurso)
+
+# formatar ----------------------------------------------------------------
+
+
+
+readr::write_csv2(precos.medios.mensal.formatado,
+                  '../saida/precos.medios.mensal.csv')
+
+precos.medios.mensal.tabela <-
+          kableExtra::kable(
+                    precos.medios.mensal.formatado,
+                    format = 'html',
+                    align = 'llllllrr',
+                    caption = epoxy::epoxy('Atualizado em {atualizacao}')
+          ) |>
+          kableExtra::kable_styling(full_width = TRUE,
+                                    bootstrap_options = 'striped')
+
+kableExtra::save_kable(precos.medios.mensal.tabela, '../saida/precos.medios.mensal.pdf')
+
+kableExtra::save_kable(precos.medios.mensal.tabela, '../saida/precos.medios.mensal.html')
+
+
+
+# semanal -----------------------------------------------------------------
+
+
+
+
+
+precos.medios.semanal.formatado <-
+          precos.medios.semanal |>
+
+atualizacao <- atualizar.monitoramento(item = 47.1)
+
+precos.medios.semanal.dados <-
+          list(atualizacao, precos.medios.semanal.formatado)
+
+readr::write_rds(precos.medios.semanal.dados,
+                 '../rds/precos.medios.semanal.dados.rds')
+
+readr::write_csv2(precos.medios.semanal.formatado,
+                  '../saida/precos.medios.semanal.csv')
+
+precos.medios.semanal.tabela <-
+          kableExtra::kable(
+                    precos.medios.semanal.formatado,
+                    format = 'html',
+                    align = 'llllllrr',
+                    caption = epoxy::epoxy('Atualizado em {atualizacao}')
+          ) |>
+          kableExtra::kable_styling(full_width = TRUE,
+                                    bootstrap_options = 'striped')
+
+kableExtra::save_kable(precos.medios.semanal.tabela, '../saida/precos.medios.semanal.pdf')
+
+kableExtra::save_kable(precos.medios.semanal.tabela, '../saida/precos.medios.semanal.html')
+
